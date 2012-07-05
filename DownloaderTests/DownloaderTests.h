f29597734f8057050aee7c5ee31d9b5ce78424b7
Aleksandr Skobelev <al.skobelev@gmail.com>
2012-07-05 14:38:09 +0400

    Initial Commit

---
+++ DownloaderTests/DownloaderTests.h
@@ -0,0 +1,13 @@
+//
+//  DownloaderTests.h
+//  DownloaderTests
+//
+//  Created by Alexander Skobelev on 05/07/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface DownloaderTests : SenTestCase
+
+@end
---
+++ DownloaderTests/DownloaderTests.m
@@ -0,0 +1,32 @@
+//
+//  DownloaderTests.m
+//  DownloaderTests
+//
+//  Created by Alexander Skobelev on 05/07/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "DownloaderTests.h"
+
+@implementation DownloaderTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in DownloaderTests");
+}
+
+@end
